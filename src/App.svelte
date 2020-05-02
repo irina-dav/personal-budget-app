b5638ee20d0744fc78709f46f32a6b81a4048a36
Irina <irina.davidenko@gmail.com>
2020-05-02 19:46:05 +0300

    Add footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,18 @@
 <script>
-	import CostsMain from './CostsMain.svelte'
+    import CostsMain from './CostsMain.svelte'
 </script>
 
-<CostsMain />
+
+<main>
+    <CostsMain/>
+</main>
+
+
+<footer class="page-footer #fafafa grey lighten-4">
+        <div class="footer-copyright grey lighten-5">
+            <div class="container">
+                <a class="grey-text text-darken-3 left" href="https://github.com/irina-dav/personal-budget-app">Personal budget app [source]</a>
+            </div>
+        </div>
+</footer>
+
